a65c2e76b2583162b347d40ac84113abee8c23f6
myh <curryhuang3627@gmail.com>
2022-04-09 21:25:32 +0800

    Initial Commit

---
+++ DrinkOrder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DrinkOrder
+//
+//  Created by Min-Yang Huang on 2022/4/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
